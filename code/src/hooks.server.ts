c9ee8256039d556ffa15d7f8c44c19cf91ad281a
kemoycampbell <ksc2650@rit.edu>
2025-03-05 07:43:47 -0500

    simpify things

--- code/src/hooks.server.ts
+++ code/src/hooks.server.ts
@@ -1,12 +1,8 @@
 //https://svelte.dev/docs/kit/hooks#Shared-hooks-init
 import 'reflect-metadata';
-import Database from '$lib/server/database/database';
-import { ENTITIES } from '$lib/server/database/entities';
-import type { HandleServerError, ServerInit } from '@sveltejs/kit';
-import dotenv from 'dotenv'; 
-import { dbInstance } from '$lib/stores/databaseStore';
-import type { Handle } from '@sveltejs/kit';
-import { UserException } from '$lib/server/exceptions/UserException';
+import type {ServerInit } from '@sveltejs/kit';
+import database from '$lib/server/database/database';
+
 
 
 export const init: ServerInit = async () => {
@@ -15,26 +11,10 @@ export const init: ServerInit = async () => {
     console.log('Server init');
 
 
-    //load the dotenv file
-    dotenv.config();
 
-    const host = process.env.DATABASE_HOST|| 'localhost';
-    const port = parseInt(process.env.DATABASE_PORT || '5432');
-    const database = process.env.DATABASE_NAME || 'sveltekit';
-    const user = process.env.DATABASE_USER || 'postgres';
-    const password = process.env.DATABASE_PASSWORD || 'password';
-    const isProduction = false;
-
-
-    //create a new instance of the database
-    const db = new Database(host, port, database, user, password, isProduction);
-    db.configure(ENTITIES, []);
-    await db.initialize();
-    console.log(ENTITIES);
-    await db.getSource().synchronize();
-
-    //we will expose the database source through the svelte store
-    dbInstance.set(db.getSource());
+    ;
+    await database.initialize();
+    database.synchronize()
 
 
 }
--- code/src/lib/server/database/database.ts
+++ code/src/lib/server/database/database.ts
@@ -1,60 +1,26 @@
-import { DataSource} from "typeorm";
-import type { MixedList } from "typeorm/browser";
 
-export default class Database {
-    private source: DataSource;
+import { DataSource, type DataSourceOptions } from "typeorm";
+import { ENTITIES } from "./entities";
 
-    constructor(
-        host: string, 
-        port: number, 
-        database: string, 
-        user: string, 
-        password: string,
-        isProduction:boolean
-        ) 
-    {
+import dotenv from "dotenv";
 
+//load the dotenv file
+dotenv.config();
 
-        this.source = new DataSource({
-            type: "postgres",
-            host: host,
-            port: port,
-            username: user,
-            password: password,
-            database: database,
-            synchronize: isProduction,
-            logging: false
-        })
-    }
+const config: DataSourceOptions = {
+    type: "postgres",
+    host: process.env.DATABASE_HOST!,
+    port: Number(process.env.DATABASE_PORT)!,
+    database: process.env.DATABASE_NAME!,
+    username: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    entities: ['src/lib/server/models/entity/**/*.ts'],
+    migrations: ['src/lib/server/database/migrations/**/*.ts'],  // Local migrations directory
+    synchronize: false,
+    logging: "all",
+};
 
-    public configure(entities: any[], migrations:MixedList<Function | string>): void {
-        this.source.setOptions({
-            entities: entities,
-            migrations: migrations
-        })
+export default new DataSource({
+    ...config
+});
 
-    }
-
-    async initialize(): Promise<void> {
-        try {
-            await this.source.initialize();
-            //TOD throw custom error
-            console.log("Database connection established.");
-        } catch (error) {
-            //throw custom error
-            console.error("Error initializing database:", error);
-            throw error; // Rethrow the error to be handled by the caller
-        }
-    }
-
-    getSource(): DataSource {
-        return this.source;
-    }
-
-    async close(): Promise<void> {
-        if (this.source.isInitialized) {
-            await this.source.destroy();
-            console.log("Database connection closed.");
-        }
-    }
-}
