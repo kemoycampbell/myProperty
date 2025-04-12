4c6c50bde6ee23fc7665aeec0450d9db5c5c6b7a
Jose <jpalominolau@gmail.com>
2025-04-11 23:17:36 -0400

    Creation of register and login pages HTML

--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <script>
-	
+	import { goto } from "$app/navigation";
+
+	let email = '';
+	let password = '';
+
+	function handleLogin() {
+		goto('/dashboard/owner/properties')
+	}
 </script>
 
 <div>
-	<div class="h1">THIS IS THE LOGIN</div>
+	<h1>THIS IS THE LOGIN</h1>
+
+	<form on:submit|preventDefault={handleLogin}>
+		<label>
+			Username:
+			<input bind:value={email} required />
+		</label>
+		<br />
+
+		<label>
+			Password:
+			<input type="password" bind:value={password} required />
+		</label>
+		<br />
+
+		<button type="submit">Login</button>
+	</form>
+	<a href="/register">Register</a>
 </div>
---
+++ code/src/routes/register/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { goto } from "$app/navigation";
+
+	let firstname = '';
+	let lastname = '';
+	let email = '';
+	let password = '';
+	let role = 'admin';
+
+	function handleRegister() {
+		goto('/dashboard/owner/properties');
+	}
+</script>
+
+<div>
+	<h1>Register</h1>
+
+	<form on:submit|preventDefault={handleRegister}>
+		<label>
+			First Name:
+			<input type="text" bind:value={firstname} required />
+		</label>
+		<br />
+
+		<label>
+			Last Name:
+			<input type="text" bind:value={lastname} required />
+		</label>
+		<br />
+
+		<label>
+			Email:
+			<input type="email" bind:value={email} required />
+		</label>
+		<br />
+
+		<label>
+			Password:
+			<input type="password" bind:value={password} required />
+		</label>
+		<br />
+
+		<button type="submit">Register</button>
+	</form>
+</div>
