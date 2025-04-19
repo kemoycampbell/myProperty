ea58c325b0fc4f8872cb55c5b9b6cceb94a6a41c
Jose <jpalominolau@gmail.com>
2025-04-19 16:56:27 -0400

    Html consuming the get all units from the property and creating new units for the desire property done

---
+++ code/src/routes/dashboard/owner/properties/[id]/unit/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { goto } from "$app/navigation";
+
+    export let data;
+
+    let unit = {
+        number: '',
+        property: data.id
+    };
+
+    async function handleSubmit() {
+        // const unitData = {
+        //     number: unit.number,
+        //     property: unit.property
+        // };
+
+        // const res = await fetch('http://localhost:5173/api/unit', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify(unitData)
+        // });
+
+        goto(`/dashboard/owner/properties/${data.id}`);
+
+        // if (res.ok) {
+        //     goto(`/dashboard/owner/properties/${data.id}`);
+        // } else {
+        //     console.error('Failed to create unit');
+        // }
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <label>
+        Number:
+        <input bind:value={unit.name} required />
+    </label>
+    <br />
+
+    <button type="submit">Create Unit</button>
+</form>
