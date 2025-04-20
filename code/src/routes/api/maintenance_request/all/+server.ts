e5ce5b08db0540a16747ebc662fcad64380abd71
Michael Berrios <mxbics@rit.edu>
2025-04-19 20:48:36 -0400

    Fixed API. Added front end fetch

--- code/src/routes/api/maintenance_request/all/+server.ts
+++ code/src/routes/api/maintenance_request/all/+server.ts
@@ -10,16 +10,17 @@ const runner = database.createQueryRunner();
 const userRepository = new UserRepository(runner);
 const maintenanceRequestRepository = new MaintenanceRequestRepository(runner);
 const unitRepository = new UnitRepository(runner);
-const service = new MaintenanceRequestService(maintenanceRequestRepository,
-    userRepository, unitRepository);
+const service = new MaintenanceRequestService(
+    maintenanceRequestRepository,
+    userRepository, 
+    unitRepository
+);
 
-export const GET = processAPIRequest(async ({ request }) => {
-    const data = await request.json();
-
+export const GET = processAPIRequest(async () => {
     const res = await service.getMaintenanceRequests();
 
     return json({
         status: 200,
         maintenanceRequests: res
-    })
+    });
 });
--- code/src/routes/dashboard/maintenance/+page.svelte
+++ code/src/routes/dashboard/maintenance/+page.svelte
@@ -1,1 +1,25 @@
-<div>THIS IS THE MAINTENANCE CONTENT PAGE</div>
+<script>
+    // Get all of the Maintenance Requests
+    async function getAllMaintenanceRequests() {
+        const res = await fetch('http://localhost:5173/api/maintenance_request/all', {
+            method: 'GET', 
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(!res.ok) {
+            console.error("Failed to retrieve Maintenance Requests");
+            return;
+        }
+
+        const data = await res.json();
+        console.log(data);
+    }
+
+    getAllMaintenanceRequests();
+</script>
+
+<div>
+    <!-- render data -->
+</div>
