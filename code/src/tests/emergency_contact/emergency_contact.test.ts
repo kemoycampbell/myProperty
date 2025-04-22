395d34fd4b3564b62b1604a7d1b1d6dd23c6007a
Jose <jpalominolau@gmail.com>
2025-04-21 23:15:29 -0400

    Finished test of emergency contact

--- code/src/tests/emergency_contact/emergency_contact.test.ts
+++ code/src/tests/emergency_contact/emergency_contact.test.ts
@@ -25,7 +25,7 @@ describe("EmergencyContactService Test", () => {
         role: fakeRole,
         createdAt: new Date(),
         updatedAt: new Date()
-    }
+    };
 
     const fakeEmergencyContact: IEmergencyContact = {
         id: "1",
@@ -36,7 +36,7 @@ describe("EmergencyContactService Test", () => {
         phone: "1231231234",
         createdAt: new Date(),
         updatedAt: new Date()
-    }
+    };
 
     beforeEach(() => {
         // Mock User Repository's findOne function
@@ -45,7 +45,9 @@ describe("EmergencyContactService Test", () => {
         } as Partial<UserRepository>;
 
         emergencyContactRepository = {
-            save: vi.fn().mockResolvedValue(fakeEmergencyContact)
+            save: vi.fn().mockResolvedValue(fakeEmergencyContact),
+            findOne: vi.fn().mockResolvedValue(fakeEmergencyContact),
+            find: vi.fn().mockResolvedValue([fakeEmergencyContact])
         } as Partial<EmergencyContactRepository>;
 
         emergencyContactService = new EmergencyContactService(
@@ -54,120 +56,186 @@ describe("EmergencyContactService Test", () => {
         );
     });
 
-    it("should throw a user exception if the tenant user id is not provided", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "",
-            fakeEmergencyContact.firstName,
-            fakeEmergencyContact.lastName,
-            fakeEmergencyContact.email,
-            fakeEmergencyContact.phone
-        );
+    describe("createEmergencyContact", () => {
+        it("should throw a user exception if the tenant user id is not provided", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "",
+                fakeEmergencyContact.firstName,
+                fakeEmergencyContact.lastName,
+                fakeEmergencyContact.email,
+                fakeEmergencyContact.phone
+            );
 
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Requested Tenant User ID is required");
-    })
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Requested Tenant User ID is required");
+        });
 
-    it("should throw a user exception if the emergency contact first name is not provided", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            "",
-            fakeEmergencyContact.lastName,
-            fakeEmergencyContact.email,
-            fakeEmergencyContact.phone
-        );
+        it("should throw a user exception if the emergency contact first name is not provided", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                "",
+                fakeEmergencyContact.lastName,
+                fakeEmergencyContact.email,
+                fakeEmergencyContact.phone
+            );
 
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid First Name");
-    })
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid First Name");
+        });
 
-    it("should throw a user exception if the emergency contact first name is less than 2 characters long", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            "a",
-            fakeEmergencyContact.lastName,
-            fakeEmergencyContact.email,
-            fakeEmergencyContact.phone
-        );
+        it("should throw a user exception if the emergency contact first name is less than 2 characters long", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                "a",
+                fakeEmergencyContact.lastName,
+                fakeEmergencyContact.email,
+                fakeEmergencyContact.phone
+            );
 
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid First Name - it is too short");
-    })
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid First Name - it is too short");
+        });
+
+        it("should throw a user exception if the emergency contact last name is not provided", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                fakeEmergencyContact.firstName,
+                "",
+                fakeEmergencyContact.email,
+                fakeEmergencyContact.phone
+            );
+
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid Last Name");
+        });
+
+        it("should throw a user exception if the emergency contact last name is less than 2 characters long", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                fakeEmergencyContact.firstName,
+                "a",
+                fakeEmergencyContact.email,
+                fakeEmergencyContact.phone
+            );
+
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid Last Name - it is too short");
+        });
+
+        it("should throw a user exception if the emergency contact email is not provided", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                fakeEmergencyContact.firstName,
+                fakeEmergencyContact.lastName,
+                "",
+                fakeEmergencyContact.phone
+            );
+
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid Email Address");
+        });
+
+        it("should throw a user exception if the emergency contact email is not a valid format", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                fakeEmergencyContact.firstName,
+                fakeEmergencyContact.lastName,
+                fakeEmergencyContact.firstName + fakeEmergencyContact.lastName + "atExample.com",
+                fakeEmergencyContact.phone
+            );
+
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid Format Email Address");
+        });
+
+        it("should throw a user exception if the emergency contact phone number is not provided", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                fakeEmergencyContact.firstName,
+                fakeEmergencyContact.lastName,
+                fakeEmergencyContact.email,
+                ""
+            );
+
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid Phone Number");
+        });
+
+        it("should throw a user exception if the emergency contact phone number is not a valid format", async () => {
+            const emergencyContact = emergencyContactService.createEmergencyContact(
+                "1",
+                fakeEmergencyContact.firstName,
+                fakeEmergencyContact.lastName,
+                fakeEmergencyContact.email,
+                "123"
+            );
+
+            await expect(emergencyContact).rejects.toThrowError(UserException);
+            await expect(emergencyContact).rejects.toThrowError("Invalid Format Phone Number");
+        });
+
+        it("should create and save the emergency contact if all fields are valid", async () => {
+            const result = await emergencyContactService.createEmergencyContact(
+                "1",
+                fakeEmergencyContact.firstName,
+                fakeEmergencyContact.lastName,
+                fakeEmergencyContact.email,
+                fakeEmergencyContact.phone
+            );
+
+            expect(result).toBeDefined();
+            expect(result).toEqual(fakeEmergencyContact);
+        });
+    });
+
+    describe("getEmergencyContactById", () => {
+        it("should throw a user exception if the emergency contact id is not provided", async () => {
+            const result = emergencyContactService.getEmergencyContactById("");
+            await expect(result).rejects.toThrowError(UserException);
+            await expect(result).rejects.toThrowError("Emergency Contact ID is required");
+        });
+
+        it("should throw a user exception if the emergency contact is not found", async () => {
+            (emergencyContactRepository.findOne as Mock).mockResolvedValue(null);
+
+            const result = emergencyContactService.getEmergencyContactById("non-existing-id");
+            await expect(result).rejects.toThrowError(UserException);
+            await expect(result).rejects.toThrowError("Emergency Contact not found");
+        });
+
+        it("should return the emergency contact if it exists", async () => {
+            (emergencyContactRepository.findOne as Mock).mockResolvedValue(fakeEmergencyContact);
+
+            const result = await emergencyContactService.getEmergencyContactById(fakeEmergencyContact.id);
+            expect(result).toBeDefined();
+            expect(result).toEqual(fakeEmergencyContact);
+        });
+    });
+
+    describe("getEmergencyContactsByUserId", () => {
+        it("should throw a user exception if the tenant user id is not provided", async () => {
+            const result = emergencyContactService.getEmergencyContactsByUserId("");
+            await expect(result).rejects.toThrowError(UserException);
+            await expect(result).rejects.toThrowError("Tenant User ID is required");
+        });
     
-    it("should throw a user exception if the emergency contact last name is not provided", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            fakeEmergencyContact.firstName,
-            "",
-            fakeEmergencyContact.email,
-            fakeEmergencyContact.phone
-        );
-
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid Last Name");
-    })
-
-    it("should throw a user exception if the emergency contact last name is less than 2 characters long", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            fakeEmergencyContact.firstName,
-            "a",
-            fakeEmergencyContact.email,
-            fakeEmergencyContact.phone
-        );
-
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid Last Name - it is too short");
-    })
-
-    it("should throw a user exception if the emergency contact email is not provided", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            fakeEmergencyContact.firstName,
-            fakeEmergencyContact.lastName,
-            "",
-            fakeEmergencyContact.phone
-        );
-
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid Email Address");
-    })
-
-    it("should throw a user exception if the emergency contact email is not a valid format", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            fakeEmergencyContact.firstName,
-            fakeEmergencyContact.lastName,
-            fakeEmergencyContact.firstName + fakeEmergencyContact.lastName + "atExample.com",
-            fakeEmergencyContact.phone
-        );
-
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid Format Email Address");
-    })
-
-    it("should throw a user exception if the emergency contact phone number is not provided", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            fakeEmergencyContact.firstName,
-            fakeEmergencyContact.lastName,
-            fakeEmergencyContact.email,
-            ""
-        );
-
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid Phone Number");
-    })
-
-    it("should throw a user exception if the emergency contact phone number is not a valid format", async() => {
-        const emergencyConctact = emergencyContactService.createEmergencyContact(
-            "1",
-            fakeEmergencyContact.firstName,
-            fakeEmergencyContact.lastName,
-            fakeEmergencyContact.email,
-            "123"
-        );
-
-        await(expect(emergencyConctact)).rejects.toThrowError(UserException);
-        await expect(emergencyConctact).rejects.toThrowError("Invalid Format Phone Number");
-    })
+        it("should return an empty array if no emergency contacts are found for the given user", async () => {
+            // Mockear el repositorio para devolver un array vacío
+            (emergencyContactRepository.find as Mock).mockResolvedValue([]);
+    
+            const result = await emergencyContactService.getEmergencyContactsByUserId("non-existing-id");
+            expect(result).toBeDefined();
+            expect(result.length).toBe(0); // Verificar que el resultado sea un array vacío
+        });
+    
+        it("should return the emergency contacts associated with the given user", async () => {
+            // Mockear el repositorio para devolver un array con un contacto de emergencia
+            (emergencyContactRepository.find as Mock).mockResolvedValue([fakeEmergencyContact]);
+    
+            const result = await emergencyContactService.getEmergencyContactsByUserId(fakeUserTenant.id);
+            expect(result).toBeDefined();
+            expect(result.length).toBeGreaterThan(0); // Verificar que el resultado contenga al menos un contacto
+            expect(result[0]).toEqual(fakeEmergencyContact); // Verificar que el primer contacto sea el esperado
+        });
+    });
 });
