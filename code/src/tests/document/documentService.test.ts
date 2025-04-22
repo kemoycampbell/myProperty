9c8638189f5d183ef71ae513b78e52631d7d2ce1
Jose <jpalominolau@gmail.com>
2025-04-21 23:35:17 -0400

    Complete test of document service

--- code/src/tests/document/documentService.test.ts
+++ code/src/tests/document/documentService.test.ts
@@ -8,36 +8,33 @@ import { DocumentService } from "$lib/server/services/documentService";
 import type { UserService } from "$lib/server/services/userService";
 import { describe, it, expect, beforeEach, vi, Mock } from "vitest";
 import fs from 'fs';
-import { base } from "$app/paths";
+import url from 'url';
+import path from 'path';
 import type { UnitService } from "$lib/server/services/unitService";
 import type { IUnit } from "$lib/server/models/entity/unit/IUnit";
 import type { IProperty } from "$lib/server/models/entity/property/IProperty";
 
+// Obtener __dirname en módulos ES
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 describe("DocumentService Tests", () => {
+    let documentService: DocumentService;
+    let documentRepository: Partial<DocumentRepository>;
+    let unitService: Partial<UnitService>;
+    let userService: Partial<UserService>;
 
-    let documentService:DocumentService;
-    let documentRepository:Partial<DocumentRepository>;
-    let unitService:Partial<UnitService>;
-    let userService:Partial<UserService>;
-
-
-    let fakeUser:IUser;
-    let fakeDocument:IDocument;
+    let fakeUser: IUser;
+    let fakeDocument: IDocument;
     let fakeUnit: IUnit;
-    let fakeProperty: IProperty
+    let fakeProperty: IProperty;
 
-
-
-    beforeEach(()=>{
-
-        //fake values
+    beforeEach(() => {
         const fakeDocType = new DocType();
         fakeDocType.name = DocumentType.LEASE_AGREEMENT;
-    
-    
+
         const fakeRole = new Role();
         fakeRole.name = RoleType.OWNER;
-    
+
         fakeDocument = {
             id: "123456",
             owner: "123456",
@@ -46,21 +43,8 @@ describe("DocumentService Tests", () => {
             createdAt: new Date(),
             updatedAt: new Date(),
             path: "",
-            docType: fakeDocType
-        }
-    
-        fakeUser = {
-            id: "123456",
-            email: "iWantToSeeTheManager@karen.com",
-            username: "karen",
-            password: "password",
-            role: fakeRole,
-            firstName: "Karen",
-            lastName: "Karenson",
-            createdAt: new Date(),
-            updatedAt: new Date()
-        } 
-
+            docType: fakeDocType,
+        };
 
         fakeUser = {
             id: "123456",
@@ -71,8 +55,8 @@ describe("DocumentService Tests", () => {
             firstName: "Karen",
             lastName: "Karenson",
             createdAt: new Date(),
-            updatedAt: new Date()
-        } 
+            updatedAt: new Date(),
+        };
 
         fakeProperty = {
             id: "123456",
@@ -84,7 +68,7 @@ describe("DocumentService Tests", () => {
             state: "test",
             zip: "12345",
             createdAt: new Date(),
-            updatedAt: new Date()
+            updatedAt: new Date(),
         };
 
         fakeUnit = {
@@ -92,164 +76,146 @@ describe("DocumentService Tests", () => {
             number: 123,
             property: fakeProperty,
             createdAt: new Date(),
-            updatedAt: new Date()
-        }
+            updatedAt: new Date(),
+        };
 
-
-
-        //we want to mock the document repository
         documentRepository = {
             save: vi.fn().mockResolvedValue(fakeDocument),
             find: vi.fn().mockResolvedValue([fakeDocument]),
             findOne: vi.fn().mockResolvedValue(fakeDocument),
-
-        }
+        };
 
         userService = {
-            getById: vi.fn().mockResolvedValue(fakeUser)
-        }
+            getById: vi.fn().mockResolvedValue(fakeUser),
+        };
 
         unitService = {
-            getById: vi.fn().mockResolvedValue(fakeUnit)
-        }
-        documentService = new DocumentService(documentRepository as DocumentRepository, userService as UserService, unitService as UnitService);
-    })
+            getById: vi.fn().mockResolvedValue(fakeUnit),
+        };
+
+        documentService = new DocumentService(
+            documentRepository as DocumentRepository,
+            userService as UserService,
+            unitService as UnitService
+        );
+    });
 
     describe("getDocumentById", () => {
-        it('should throw an error if id is not provided', async () => {
+        it("should throw an error if id is not provided", async () => {
             const id = "";
-
             const result = documentService.getDocumentById(id);
-            await expect(result).rejects.toThrowError(
-                new UserException("Document ID is required", 400)
-            );
+            await expect(result).rejects.toThrowError(new UserException("Document ID is required", 400));
         });
 
-        it('should throw an error if document is not found', async () => {
-            //mock the repository to return null
+        it("should throw an error if document is not found", async () => {
             (documentRepository.findOne as Mock).mockResolvedValueOnce(null);
-
             const id = "123";
             const result = documentService.getDocumentById(id);
-            await expect(result).rejects.toThrowError(
-                new UserException("Document not found", 400)
-            );
+            await expect(result).rejects.toThrowError(new UserException("Document not found", 400));
         });
 
-        it('should return the document if found', async () => {
+        it("should return the document if found", async () => {
             const id = "123456";
             const result = await documentService.getDocumentById(id);
             expect(result).toBeDefined();
             expect(result.id).toBe(id);
-
             expect(result).toBe(fakeDocument);
         });
-    })
+    });
 
     describe("getDocumentsByUserId", () => {
-        it('should throw an error if userId is not provided', async () => {
+        it("should throw an error if userId is not provided", async () => {
             const userId = "";
-
             const result = documentService.getDocumentsByUserId(userId);
-            await expect(result).rejects.toThrowError(
-                new UserException("User ID is required", 400)
-            );
+            await expect(result).rejects.toThrowError(new UserException("User ID is required", 400));
         });
 
-        it('should throw an error if user is not found', async () => {
-            //mock the repository to return null
+        it("should throw an error if user is not found", async () => {
             (userService.getById as Mock).mockResolvedValueOnce(null);
-
             const userId = "123";
             const result = documentService.getDocumentsByUserId(userId);
-            await expect(result).rejects.toThrowError(
-                new UserException("User not found", 400)
-            );
+            await expect(result).rejects.toThrowError(new UserException("User not found", 400));
         });
 
-        it('should return the documents if found', async () => {
+        it("should return the documents if found", async () => {
             const userId = "123456";
             const result = await documentService.getDocumentsByUserId(userId);
             expect(result).toBeDefined();
             expect(result[0].owner).toBe(userId);
-
             expect(result[0]).toBe(fakeDocument);
         });
     });
 
     describe("upload", () => {
-        it('should throw an error if file is not provided', async () => {
+        it("should throw an error if file is not provided", async () => {
             const file = "";
-
             const result = documentService.upload(file);
             await expect(result).rejects.toThrowError(
                 new UserException("file is required!", 400)
             );
         });
 
-        it('should throw an error if file type is not allowed', async () => {
-            const file = "IA=="; //not a valid type
-
+        it("should throw an error if file type is not allowed", async () => {
+            const file = "IA=="; // Base64 no válido
             const result = documentService.upload(file);
             await expect(result).rejects.toThrowError(
-                new UserException("File type not allowed. Allowed file types are: application/pdf, image/jpeg, image/png, application/msword, application/docx", 400)
+                new UserException(
+                    "File type not allowed. Allowed file types are: application/pdf, image/jpeg, image/png, application/msword, application/docx",
+                    400
+                )
             );
         });
-        
-        it('should return the document if file is valid', async () => {
-            const base64File = fs.readFileSync("src/tests/document/lease.txt", 'utf8');
-        
+
+        it("should return the document if file is valid", async () => {
+            const leasePath = path.join(__dirname, "lease.txt");
+            const base64File = fs.readFileSync(leasePath, "utf8");
             const result = await documentService.upload(base64File);
             const normalizedResult = result.replace(/\\/g, "/");
-        
-            expect(result).toBeDefined();   
+            expect(result).toBeDefined();
             expect(normalizedResult).toContain("uploads/");
-            expect(normalizedResult).toContain(".pdf");
-        });        
+            expect(normalizedResult).toMatch(/\.(pdf|jpeg|jpg|png|doc|docx)$/);
+        });
     });
 
     describe("createDocument", () => {
-        it('should throw an error if the owner id is not provided', async () => {
+        it("should throw an error if the owner id is not provided", async () => {
             const owner = "";
             const tenant = "123";
             const unit = "123";
-            const base64File = fs.readFileSync("src/tests/document/lease.txt", 'utf8');
-
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError(UserException);
-
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError("Owner id is required");
+            const base64File = "base64-valid-file";
+            await expect(documentService.createDocument(owner, tenant, unit, base64File)).rejects.toThrowError(
+                new UserException("Owner id is required", 400)
+            );
         });
 
-        it('should throw an error if the tenant id is not provided', async () => {
+        it("should throw an error if the tenant id is not provided", async () => {
             const owner = "123";
             const tenant = "";
             const unit = "123";
-            const base64File = fs.readFileSync("src/tests/document/lease.txt", 'utf8');
-
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError(UserException);
-
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError("Tenant id is required");
+            const base64File = "base64-valid-file";
+            await expect(documentService.createDocument(owner, tenant, unit, base64File)).rejects.toThrowError(
+                new UserException("Tenant id is required", 400)
+            );
         });
 
-        it('should throw an error if the unit id is not provided', async () => {
+        it("should throw an error if the unit id is not provided", async () => {
             const owner = "123";
             const tenant = "123";
             const unit = "";
-            const base64File = fs.readFileSync("src/tests/document/lease.txt", 'utf8');
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError(UserException);
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError("Unit id is required");
+            const base64File = "base64-valid-file";
+            await expect(documentService.createDocument(owner, tenant, unit, base64File)).rejects.toThrowError(
+                new UserException("Unit id is required", 400)
+            );
         });
 
-        it('should throw an error if the file is not provided', async () => {
+        it("should throw an error if the file is not provided", async () => {
             const owner = "123";
             const tenant = "123";
             const unit = "123";
             const base64File = "";
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError(UserException);
-            await expect(documentService.createDocument(owner, tenant, unit, base)).rejects.toThrowError("file is required!");
-
+            await expect(documentService.createDocument(owner, tenant, unit, base64File)).rejects.toThrowError(
+                new UserException("file is required!", 400)
+            );
         });
     });
-
-   
 });
